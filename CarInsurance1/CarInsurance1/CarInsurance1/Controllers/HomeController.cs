85f8c83c4657c0973fcf31038fa8f7bce2294ab8
Rae <rae@gmail.com>
2019-08-15 16:26:30 -0700

    Create repository for Car Insurance Project

---
+++ CarInsurance1/CarInsurance1/CarInsurance1/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System.Web.Mvc;
+using CarInsurance1.Models;
+using CarInsurance1.Controllers;
+
+
+namespace CarInsurance1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private object insuranceQuote;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        int CalculateAge(System.DateTime dateOfBirth, System.DateTime now)
+        {
+            int age = now.Year - dateOfBirth.Year;
+
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                age--;
+
+            return age;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult InsuranceQuote(string firstName, string lastName, string emailAddress, int age, string carMake, string carModel, int carYear, bool dui, bool fullCoverage, int speedTickets)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel))
+            {
+                return View("Error");
+            }
+
+            decimal quote = 50;
+            decimal duiPercentage = .25m;
+            decimal addFullCoverage = .5m;
+
+
+          
+
+
+            if (age < 25)
+            {
+                quote += 25;
+            }
+
+            else if (age < 18)
+            {
+                quote += 100;
+            }
+
+            else if (age > 100)
+            {
+                quote += 25;
+            }
+
+
+            if (carYear < 2000)
+            {
+                quote += 25;
+            }
+
+            else if (carYear > 2015)
+            {
+                quote += 25;
+            }
+
+
+            if (carMake == "Porsche")
+            {
+                quote += 25;
+            }
+
+            else if (carMake == "Porsche" && carModel == "Carerra 911")
+            {
+                quote += 25;
+            }
+
+
+            if (dui == true)
+            {
+                quote *= duiPercentage;
+            }
+
+            if (speedTickets > 0)
+            {
+                quote += speedTickets * 10;
+            }
+
+            if (fullCoverage == true)
+            {
+                quote *= addFullCoverage;
+            }
+
+            else
+            {
+
+                using (InsuranceEntities db = new InsuranceEntities())
+                {
+                    var insuranceQuote = new Insuree();
+                    insuranceQuote.FirstName = firstName;
+                    insuranceQuote.LastName = lastName;
+                    insuranceQuote.EmailAddress = emailAddress;
+                    insuranceQuote.CarMake = carMake;
+                    insuranceQuote.CarYear = carYear;
+                    insuranceQuote.DUI = dui;
+                    insuranceQuote.CoverageType = fullCoverage;
+                    insuranceQuote.SpeedingTickets = speedTickets;
+                    insuranceQuote.Age = age;
+                    insuranceQuote.Quote = quote;
+
+                    //db.InsuranceQuotes.Add(insuranceQuote);
+                    db.Insurees.Add(insuranceQuote);
+                    db.SaveChanges();
+                }
+
+                
+            }
+            return View("Success");
+        }
+
+     }
+}
---
+++ CarInsurance1/CarInsurance1/CarInsurance1/Models/InsuranceQuote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance1.Models
+{
+    public class InsuranceQuote
+    {
+        public int age { get; set; }
+        public string carMake { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string EmailAddress { get; internal set; }
+        public int carYear { get; internal set; }
+        public bool Dui { get; internal set; }
+        public bool fullCoverage { get; internal set; }
+        public int speedTicket { get; internal set; }
+        public decimal quote { get; set; }
+    }
+}
