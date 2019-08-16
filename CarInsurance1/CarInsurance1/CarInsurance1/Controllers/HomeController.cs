651b15a9447d96dac235408f5b34990b4eb64086
Rae <rae@gmail.com>
2019-08-15 17:46:42 -0700

    Update calculateAge Method

--- CarInsurance1/CarInsurance1/CarInsurance1/Controllers/HomeController.cs
+++ CarInsurance1/CarInsurance1/CarInsurance1/Controllers/HomeController.cs
@@ -15,12 +15,14 @@ namespace CarInsurance1.Controllers
         }
 
 
-        int CalculateAge(System.DateTime dateOfBirth, System.DateTime now)
+        public int CalculateAge(System.DateTime dateOfBirth)
         {
-            int age = now.Year - dateOfBirth.Year;
+            int age = 0;
+            age = System.DateTime.Now.Year - dateOfBirth.Year;
 
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
-                age--;
+
+            if (System.DateTime.Now.Month < dateOfBirth.Month || (System.DateTime.Now.Month == dateOfBirth.Month && System.DateTime.Now.Day < dateOfBirth.Day))
+                age -= 1;
 
             return age;
         }
@@ -28,76 +30,75 @@ namespace CarInsurance1.Controllers
 
 
         [HttpPost]
-        public ActionResult InsuranceQuote(string firstName, string lastName, string emailAddress, int age, string carMake, string carModel, int carYear, bool dui, bool fullCoverage, int speedTickets)
+        public ActionResult InsuranceQuote(string firstName, string lastName, string emailAddress, System.DateTime dateOfBirth, string carMake, string carModel, int carYear, bool dui, bool fullCoverage, int speedTickets)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel))
             {
                 return View("Error");
             }
-
-            decimal quote = 50;
-            decimal duiPercentage = .25m;
-            decimal addFullCoverage = .5m;
-
-
-          
-
-
-            if (age < 25)
-            {
-                quote += 25;
-            }
-
-            else if (age < 18)
-            {
-                quote += 100;
-            }
-
-            else if (age > 100)
-            {
-                quote += 25;
-            }
-
-
-            if (carYear < 2000)
-            {
-                quote += 25;
-            }
-
-            else if (carYear > 2015)
-            {
-                quote += 25;
-            }
-
-
-            if (carMake == "Porsche")
-            {
-                quote += 25;
-            }
-
-            else if (carMake == "Porsche" && carModel == "Carerra 911")
-            {
-                quote += 25;
-            }
-
-
-            if (dui == true)
-            {
-                quote *= duiPercentage;
-            }
-
-            if (speedTickets > 0)
-            {
-                quote += speedTickets * 10;
-            }
-
-            if (fullCoverage == true)
-            {
-                quote *= addFullCoverage;
-            }
-
             else
             {
+                decimal quote = 50;
+                decimal duiPercentage = .25m;
+                decimal addFullCoverage = .5m;
+                int age = CalculateAge(dateOfBirth);
+
+
+
+
+                if (age < 25)
+                {
+                    quote += 25;
+                }
+
+                else if (age < 18)
+                {
+                    quote += 100;
+                }
+
+                else if (age > 100)
+                {
+                    quote += 25;
+                }
+
+
+                if (carYear < 2000)
+                {
+                    quote += 25;
+                }
+
+                else if (carYear > 2015)
+                {
+                    quote += 25;
+                }
+
+
+                if (carMake == "Porsche")
+                {
+                    quote += 25;
+                }
+
+                else if (carMake == "Porsche" && carModel == "Carerra 911")
+                {
+                    quote += 25;
+                }
+
+
+                if (dui == true)
+                {
+                    quote *= duiPercentage;
+                }
+
+                if (speedTickets > 0)
+                {
+                    quote += speedTickets * 10;
+                }
+
+                if (fullCoverage == true)
+                {
+                    quote *= addFullCoverage;
+                }
+
 
                 using (InsuranceEntities db = new InsuranceEntities())
                 {
@@ -110,7 +111,7 @@ namespace CarInsurance1.Controllers
                     insuranceQuote.DUI = dui;
                     insuranceQuote.CoverageType = fullCoverage;
                     insuranceQuote.SpeedingTickets = speedTickets;
-                    insuranceQuote.Age = age;
+                    insuranceQuote.DateOfBirth = dateOfBirth;
                     insuranceQuote.Quote = quote;
 
                     //db.InsuranceQuotes.Add(insuranceQuote);
@@ -118,10 +119,11 @@ namespace CarInsurance1.Controllers
                     db.SaveChanges();
                 }
 
-                
-            }
-            return View("Success");
-        }
 
+
+                return View("Success");
+            }
+
+        }
      }
 }
